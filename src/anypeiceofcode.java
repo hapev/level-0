0fdcf5cfcb9f84b008d7ddef8f6879d5a3976d12
League <jiwenzou@gmail.com>
2016-10-16 12:59:04 -0700

    h

---
+++ src/anypeiceofcode.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class anypeiceofcode {
+public static void main(String[] args) {
+String Name == JOptionPane.showInputDialog("What is your name?");
+if (Name.equals("Evan")){
+	System.out.println("You have a great name.");
+}
+else{
+	system.out.println("You have an awful name.");
+	}
+Int Age = JOptionPane.showinputDialog("What is your age?");
+if (Age.equals("13")){
+	System.out.println("You have a great age.");
+}
+else{
+	System.out.println(You have an awful age.);
+	}
+
+	
+	
+	
+	
+	
+	
+}
+}
--- src/tresnumeros.java
+++ src/tresnumeros.java
@@ -1,85 +1,57 @@
 import javax.swing.JOptionPane;
 
 public class tresnumeros {
-public static void main(String[] args) {
-	String firstnumber = JOptionPane.showInputDialog("What is your number");
-	String secondnumber = JOptionPane.showInputDialog("What is your second number");
-	String thirdnumber = JOptionPane.showInputDialog("What is your third number");
-	 int number1 = Integer.parseInt(firstnumber);
-	int number2 = Integer.parseInt(secondnumber);
-	int number3 = Integer.parseInt(thirdnumber);
-	int HighestNumber = 0;
-	int LowestNumber = 0;
-	int MiddleNumber = 0;
-	
-	
-	
-//	
-	
-	
-	if (number1 > number2&&number1 > number3){ 
-		HighestNumber = 1;
-	}
-	else
-	{
-		if (number1 > number2 && number1 < number3||number1 < number2 && number1 > number3){
-			MiddleNumber = 1;
-			
-			
+	public static void main(String[] args) {
+		String firstnumber = JOptionPane.showInputDialog("What is your number");
+		String secondnumber = JOptionPane.showInputDialog("What is your second number");
+		String thirdnumber = JOptionPane.showInputDialog("What is your third number");
+		int number1 = Integer.parseInt(firstnumber);
+		int number2 = Integer.parseInt(secondnumber);
+		int number3 = Integer.parseInt(thirdnumber);
+		int HighestNumber = 0;
+		int LowestNumber = 0;
+		int MiddleNumber = 0;
+
+		if (number1 > number2 && number1 > number3) {
+			HighestNumber = number1;
+		} else {
+			if (number1 > number2 && number1 < number3 || number1 < number2 && number1 > number3) {
+				MiddleNumber = number1;
+
+			} else {
+				LowestNumber = number1;
+
+			}
+
 		}
-		else{
-			LowestNumber = 1;
-			
-			
+		//
+
+		if (number2 > number1 && number2 > number3) {
+			HighestNumber = number2;
+		} else {
+			if (number2 > number1 && number2 < number3 || number2 < number1 && number2 > number3) {
+				MiddleNumber = number2;
+			} else {
+				LowestNumber = number2;
+			}
 		}
-		
-		
-	}
-	
-	
-//	
-	
-	
-	
-	if (number2 > number1&&number2 > number3){ 
-		HighestNumber = 2;
-	}
-	else
-	{
-		if (number2 > number1 && number2 < number3||number2 < number1 && number2 > number3){
-			MiddleNumber = 2;
-			
-			
+
+		if (number3 > number1 && number3 > number2) {
+			HighestNumber = number3;
+		} else {
+			if (number3 > number1 && number3 < number2 || number3 < number1 && number3 > number2) {
+				MiddleNumber = number3;
+			} else {
+				LowestNumber = number3;
+			}
 		}
-		else{
-			LowestNumber = 2;
-			
-			
-		}
-		
-		
+
+		System.out.println(LowestNumber);
+		System.out.println(MiddleNumber);
+		System.out.println(HighestNumber);
+		System.out.println(HighestNumber);
+		System.out.println(MiddleNumber);
+		System.out.println(LowestNumber);
+
 	}
-	
-//
-	if (number3 > number1&&number3 > number2){ 
-		HighestNumber = 2;
-	}
-	else
-	{
-		if (number3 > number1 && number3 < number2||number3 < number1 && number3 > number2){
-			MiddleNumber = 2;
-			
-			
-		}
-		else{
-			LowestNumber = 2;
-			
-			
-		}
-		
-		
-	}
-	
-	
-}
 }
