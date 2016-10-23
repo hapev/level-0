7083c4ce4c118a8e6afc28ab372ede8fbc5acc4c
League <jiwenzou@gmail.com>
2016-10-23 12:57:17 -0700

    h

---
+++ src/forloopgauntlet.java
@@ -0,0 +1,26 @@
+
+public class forloopgauntlet {
+public static void main(String[] args) {
+for (int x = 0; x<100;x++){
+	System.out.println(x);
+}
+for (int a = 100;a> 0;a--){
+	System.out.println(a);
+}
+for (int l = 2;l<100;l=l+2){
+System.out.println(l);
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
