cca19c054640f1f8aa61c412fb4d9750d1e3e4ed
dtak7 <league@WTS13.attlocal.net>
2016-10-30 12:59:48 -0700

    h

--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,10 +1,20 @@
-import javax.swing.JOptionPane;
+import java.util.Calendar;
 
+import javax.swing.JOptionPane;
 public class CodingExercise2 {
+
+
+
+
 public static void main(String[] args) {
-	String age0 = JOptionPane.showInputDialog("What is your age?");
+	Calendar now = Calendar.getInstance();
+
+     String age0 = JOptionPane.showInputDialog("What is your age?");
 	int age = Integer.parseInt(age0);
-	int year = 2016-age;
+		now.add(Calendar.YEAR, -age);
+		
+		
+		int year = now.get(Calendar.YEAR);
 	JOptionPane.showMessageDialog(null, "You were born in "+year+".");
 	if (age>30){
 		JOptionPane.showMessageDialog(null, "You are too old to play this game.");
